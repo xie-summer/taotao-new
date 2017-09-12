fa2601dd2e7abd2f73207286871d232e106db556
sunder-xie <xieshengrong@live.com>
2017-09-12 22:21:58 +0800

    FilterInvocationSecurityMetadataSource  已经有实现类了 GewaraUserMapper 实现User model

--- taotao-sso-dao/src/main/java/com/taotao/mapper/GewaraUserMapper.java
+++ taotao-sso-dao/src/main/java/com/taotao/mapper/GewaraUserMapper.java
@@ -1,13 +1,14 @@
 package com.taotao.mapper;
 
 import com.taotao.model.acl.GewaraUser;
+import com.taotao.model.acl.User;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
 @Repository
-public interface GewaraUserMapper extends Mapper<GewaraUser> {
+public interface GewaraUserMapper extends Mapper<User> {
     @Select(" select * from user u where u.username =#{name}")
     GewaraUser findByUserName(@Param("name")String name);
 }
