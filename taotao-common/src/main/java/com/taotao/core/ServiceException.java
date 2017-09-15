2221dba47738ccda298bf6582a42e78737cfd49d
sunder-xie <xieshengrong@live.com>
2017-09-15 10:24:01 +0800

    优化依赖

--- taotao-common/src/main/java/com/taotao/core/ServiceException.java
+++ taotao-common/src/main/java/com/taotao/core/ServiceException.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 /**
  * 服务（业务）异常如“ 账号或密码错误 ”，该异常只做INFO级别的日志记录 @see WebMvcConfigurer
  */
-
+@Service
 public class ServiceException extends RuntimeException {
     public ServiceException() {
     }
